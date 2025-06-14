94257e4245f9c268dc3267208220072575c1c93c
liulan1993 <plj453369670@gmail.com>
2025-06-14 15:05:30 +0800

    123321

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1092,12 +1092,12 @@ function ComponentTwentyMedicalHealth() {
                 </div>
               </div>
               <img
-                src="https://www.twblocks.com/_next/image?url=%2Fdarkmode-feature8.png&w=3840&q=75" alt="处理后效果" width={1920} height={1080}
+                src="https://raw.githubusercontent.com/liulan1993/apex-company-website/refs/heads/main/public/1.png" alt="处理后效果" width={1920} height={1080}
                 className="absolute left-0 top-0 z-10 w-full h-full object-cover rounded-2xl select-none border"
                 style={{ clipPath: `inset(0 ${100 - inset}% 0 0)` }} draggable={false}
               />
               <img
-                src="https://www.twblocks.com/_next/image?url=%2Ffeature8.png&w=3840&q=75" alt="处理前原图" width={1920} height={1080}
+                src="https://raw.githubusercontent.com/liulan1993/apex-company-website/refs/heads/main/public/2.png" alt="处理前原图" width={1920} height={1080}
                 className="absolute left-0 top-0 w-full h-full object-cover rounded-2xl select-none border" draggable={false}
               />
             </div>
